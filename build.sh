704c3cabec9679b7ee096c5ac5e6add7dbbdb5ca
Howell Yang <th15817161961@gmail.com>
2023-01-28 13:41:08 +0800

    feat: build shell

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# step 1
+./bootstrap.sh
+
+# step 2
+export freeswitch_base_path=$(pwd)
+./configure --prefix=$freeswitch_base_path/All/Debug
+
+# step 3
+make clean && make -j && make -j install
